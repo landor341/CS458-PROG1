be76cd334f3eee60a6837a8c234482eaed77be1c
Landon <larobis4@gmail.com>
2024-02-11 15:14:11 -0600

    Defined constructor and added temp test prints

--- src/Instructions/JInstruction.java
+++ src/Instructions/JInstruction.java
@@ -1,8 +1,15 @@
 package Instructions;
 
+import enums.OP;
+
 public class JInstruction extends Word {
     // Format of J Instruction:
     //    31-26        25-0
     //    opcode       address
     // On instantiation make address an optional field since if it uses a label we might not know what address to set it to yet
+
+    public JInstruction(OP o, String address) {
+        // throw error if address isn't valid
+        System.out.println("RInstruction " + o.name + " " + address); // TODO: Remove. Debug line
+    }
 }
--- src/Instructions/RInstruction.java
+++ src/Instructions/RInstruction.java
@@ -1,7 +1,14 @@
 package Instructions;
 
+import enums.OP;
+
 public class RInstruction extends Word {
     // Format of R Instruction: (Opcode is always zero iirc)
     //    31-26        25-21     20-16      15-11     10-6      5-0
     //    opcode      $rs         $rt        $rd      shamt     funct
+
+    public RInstruction (OP o, String rt, String rd, String rsOrShamt) {
+        // Throw error is rs, rt, and rdOrShamt don't map to valid REG enums
+        System.out.println("RInstruction " + o.name + " " + rt + " " + rd + " " + rsOrShamt); // TODO: Remove. Debug line
+    }
 }
